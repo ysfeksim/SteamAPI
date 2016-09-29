e5646ffb914eef439b859e1d167deeb94912d118
tunahan erkoyuncu <erkoyuncu@itu.edu.tr>
2016-09-29 12:54:37 +0300

    tam hali

---
+++ Tag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAPI
+{
+    class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+    }
+}
